e60aebc040bf710130d3c08f0ae66959398dfb3f
vanamalagithub <vanamalachinnarappabz@gmail.com>
2023-06-12 14:38:46 +0530

    DBUtils.java

--- DBUtils.java
+++ DBUtils.java
@@ -1,2 +1,7 @@
 Hello guys
 Git commands are usefull
+Hello 
+this is updated by chinna
+good afternoon
+
+
--- stringutils.java
+++ stringutils.java
@@ -1,2 +1,6 @@
 Hello this file contails the stringutils
+Good morning
+good afternoon
+good evening
+
 
